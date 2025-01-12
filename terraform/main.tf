95c77e3b2ad3d8407abcea22b15cc7d1b4847bd4
tvdven <threehook@hotmail.com>
2025-01-12 12:58:03 +0100

    feat: write kubeconfig

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,7 +16,6 @@ terraform {
 # Configure the GitHub Provider
 provider "github" {}
 
-
 # provider "kubernetes" {
 #   config_path = var.kube_config
 #
@@ -24,7 +23,7 @@ provider "github" {}
 #     manifest_resource = true
 #   }
 # }
-#
+
 # provider "helm" {
 #   kubernetes {
 #     config_path = var.kube_config

a57d2d94f840cc8ad18163c3abbf0269f0d3db88
Ton van de Ven <threehook@users.noreply.github.com>
2025-01-16 14:52:36 +0100

    K8s internals (#82)
    * feat: add pull secret
    
    * feat: add pull secret
    
    * feat: add pull secret
    
    * feat: write kubeconfig
    
    * feat: write kubeconfig
    
    * feat: write kubeconfig
    
    * feat: write kubeconfig
    
    * feat: write kubeconfig
    
    * feat: write kubeconfig
    
    * feat: federated identity sp-terraform
    
    * feat: federated identity sp-terraform
    
    * feat: federated identity sp-terraform
    
    * feat: federated identity sp-terraform
    
    * feat: federated identity sp-terraform
    
    * feat: write kube config to fs
    
    * feat: write kube config to fs
    
    * feat: write kube config to fs
    
    * feat: write kube config to fs
    
    * feat: write kube config to fs
    
    * feat: create pull secret
    
    * feat: create pull secret
    
    * feat: add k8s namespace via terraform
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,6 +34,12 @@ data "azurerm_kubernetes_cluster" "credentials" {
 
 provider "kubernetes" {
   config_path            = var.kube_config
+  host                   = data.azurerm_kubernetes_cluster.credentials.kube_config[0].host
+  username               = data.azurerm_kubernetes_cluster.credentials.kube_config[0].username
+  password               = data.azurerm_kubernetes_cluster.credentials.kube_config[0].password
+  client_certificate     = base64decode(data.azurerm_kubernetes_cluster.credentials.kube_config[0].client_certificate, )
+  client_key             = base64decode(data.azurerm_kubernetes_cluster.credentials.kube_config[0].client_key, )
+  cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.credentials.kube_config[0].cluster_ca_certificate, )
 }
 
 provider "helm" {

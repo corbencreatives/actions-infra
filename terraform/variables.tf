70881bb9ac272f2207d160ff7c903a66824bff81
tvdven <threehook@hotmail.com>
2025-01-16 14:59:13 +0100

    feat: set provider kubernetes

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -31,5 +31,5 @@ variable "ARM_CLIENT_SECRET" {
 variable "kube_config" {
   type        = string
   description = "The kubernetes config"
-  default = "~/.kube/config"
+  default = "kube_config"
 }

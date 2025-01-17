9854eabaf85fe362e502bc8a35c7588f8238c450
Ton van de Ven <threehook@users.noreply.github.com>
2025-01-17 13:34:36 +0100

    K8s internals (#96)
    * feat: add pull secret
    
    * feat: add pull secret
    
    * feat: add pull secret
    
    * feat: write kubeconfig
    
    * feat: write kubeconfig
    
    * feat: write kubeconfig
    
    * feat: write kubeconfig
    
    * feat: write kubeconfig
    
    * feat: write kubeconfig
    
    * feat: federated identity sp-terraform
    
    * feat: federated identity sp-terraform
    
    * feat: federated identity sp-terraform
    
    * feat: federated identity sp-terraform
    
    * feat: federated identity sp-terraform
    
    * feat: write kube config to fs
    
    * feat: write kube config to fs
    
    * feat: write kube config to fs
    
    * feat: write kube config to fs
    
    * feat: write kube config to fs
    
    * feat: create pull secret
    
    * feat: create pull secret
    
    * feat: add k8s namespace via terraform
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes
    
    * feat: set provider kubernetes
    
    * feat: set registrypullsecret
    
    * feat: set registrypullsecret
    
    * feat: set registrypullsecret
    
    * feat: set registrypullsecret
    
    * feat: set registrypullsecret
    
    * feat: set registrypullsecret
    
    * feat: set registrypullsecret
    
    * feat: set registrypullsecret
    
    * feat: set registrypullsecret
    
    * feat: set registrypullsecret

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -43,6 +43,17 @@ resource "kubernetes_namespace" "cicd-namespace" {
   }
 }
 
+data "template_file" "docker_config_script" {
+  template = file("${path.module}/docker_config.json")
+  vars = {
+    docker-username           = var.docker_username
+    docker-password           = var.docker_password
+    docker-server             = var.docker_server
+    #     docker-email              = "${var.docker-email}"
+    auth                      = base64encode("${var.docker_username}:${var.docker_password}")
+  }
+}
+
 resource "kubernetes_secret" "docker-registry" {
   metadata {
     name = "registrypullsecret"
@@ -54,13 +65,4 @@ resource "kubernetes_secret" "docker-registry" {
   type = "kubernetes.io/dockerconfigjson"
 }
 
-data "template_file" "docker_config_script" {
-  template = file("./docker_config.json")
-  vars = {
-    docker-username           = var.docker_username
-    docker-password           = var.docker_password
-    docker-server             = var.docker_server
-#     docker-email              = "${var.docker-email}"
-#     auth                      = base64encode("${var.docker_username}:${var.docker_password}")
-  }
-}
+

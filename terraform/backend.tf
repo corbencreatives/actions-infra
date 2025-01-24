9c2032e596553f6cb5fd5724ab26619ff0929457
tvdven <threehook@hotmail.com>
2025-01-24 09:28:57 +0100

    feat: call tf-plan-apply

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -48,7 +48,7 @@ resource "kubernetes_secret" "registry-pull-secret" {
       auths = {
         (var.docker_server) = {
           auth = base64encode(format("%s:%s",
-            var.DOCKER_USERNAME, var.DOCKER_PASSWORD
+            var.docker_username, var.docker_password
           ))
         }
       }
